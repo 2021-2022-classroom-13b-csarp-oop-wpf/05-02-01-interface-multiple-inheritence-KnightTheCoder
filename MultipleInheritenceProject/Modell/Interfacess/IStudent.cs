b76826ce26e467d8bddd73bebae9a3c3f2499a80
KocsisDominikAdrian <sz1kocdom@vasvari.org>
2022-05-09 15:20:06 +0200

    Diák kész

--- MultipleInheritenceProject/Modell/Interfacess/IStudent.cs
+++ MultipleInheritenceProject/Modell/Interfacess/IStudent.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Kreta.Models.Interfaces.Base;
 
 namespace Kreta.Models.Interfaces
 {
-    interface IStudent 
+    interface IStudent : IPerson, IAddress, IAccount
     {
+        public int StudentID { get; set; }
     }
 }
